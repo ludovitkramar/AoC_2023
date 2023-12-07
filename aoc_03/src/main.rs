103bff363c24dc35b89573d2afb65e566533f4ea
Ludovit <ludovit.kramar@pilotgaea.com.tw>
2023-12-07 15:19:48 +0800

    Day 3 second part

--- aoc_03/src/main.rs
+++ aoc_03/src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+
+// (start, end, value), at end index it's something else.
 fn get_numbers_from_line(line: &str) -> Vec<(usize, usize, u32)> {
     let mut numbers: Vec<(usize, usize, u32)> = Vec::new();
 
@@ -25,7 +28,7 @@ fn get_numbers_from_line(line: &str) -> Vec<(usize, usize, u32)> {
     // if line ends with a number
     if matching_number {
         let num = line.get(start_index..).unwrap().parse::<u32>().unwrap();
-        numbers.push((start_index, line.len() - 1, num));
+        numbers.push((start_index, line.len(), num));
     }
 
     return numbers;
@@ -60,7 +63,15 @@ fn get_indices_around_region(start: usize, end: usize, line: usize) -> Vec<(usiz
 
 fn main() {
     let input = include_str!("input");
+    let sum = do_part_one(input);
 
+    println!("Result: {}", sum);
+
+    let part_2 = part_two(input);
+    println!("Part two result: {}", part_2);
+}
+
+fn do_part_one(input: &str) -> u32 {
     let lines: Vec<&str> = input.lines().collect();
     let mut sum = 0;
 
@@ -72,7 +83,6 @@ fn main() {
 
         for number in numbers {
             let indices = get_indices_around_region(number.0, number.1, i);
-            println!("Number: {}, Indices: {:?}", number.2, indices);
             for position in indices {
                 let x = lines.get(position.0);
                 match x {
@@ -82,7 +92,6 @@ fn main() {
                             Some(y) => {
                                 if y != '.' && !y.is_numeric() {
                                     sum += number.2;
-                                    println!("Adding: {}", number.2);
                                     break;
                                 }
                             }
@@ -95,6 +104,104 @@ fn main() {
         }
     }
 
-    // assert_eq!(sum, 4361);
-    println!("Result: {}", sum);
+    sum
+}
+
+fn part_two(input: &str) -> u32 {
+    // Add up all the mulpiples of number around a gear. (Only when there are exactly two numbers around a gear)
+    // 467..114..
+    // ...*......
+    // ..35..633.
+    // ......#...
+    // 617*......
+    // .....+.58.
+    // ..592.....
+    // ......755.
+    // ...$.*....
+    // .664.598..
+
+    let mut sum = 0;
+    let mut numbers = Vec::new();
+
+    for line in input.lines() {
+        let nums = get_numbers_from_line(line);
+        numbers.push(nums);
+    }
+
+    for (i, line) in input.lines().enumerate() {
+        for (j, char) in line.chars().enumerate() {
+            if char == '*' {
+                let to_check = get_indices_around_region(j, j + 1, i);
+
+                let mut matches = HashSet::new();
+
+                for index in to_check {
+                    let nums = numbers.get(index.0);
+                    if nums.is_some() {
+                        let nums = nums.unwrap();
+                        for num in nums {
+                            let start = num.0;
+                            let end = num.1;
+
+                            if index.1 >= start && index.1 < end {
+                                matches.insert(num);
+                            }
+                        }
+                    }
+                }
+
+                let mut mult = 0;
+
+                if matches.len() == 2 {
+                    for val in matches.into_iter() {
+                        if mult == 0 {
+                            mult = val.2;
+                        } else {
+                            mult *= val.2;
+                        }
+                    }
+                }
+
+                sum += mult;
+            }
+        }
+    }
+
+    sum
+}
+
+#[test]
+fn test() {
+    let example = include_str!("example");
+    let example_sum = do_part_one(example);
+
+    assert_eq!(example_sum, 4361);
+
+    let input = include_str!("input");
+    let input_sum = do_part_one(input);
+    assert_eq!(input_sum, 525181);
+
+    // part_two example should be: 467835
+    let example_2 = part_two(example);
+    assert_eq!(example_2, 467835);
+}
+
+#[test]
+fn test_n_from_line() {
+    let a = get_numbers_from_line("....123");
+    let b = get_numbers_from_line("...456.");
+
+    assert!(a.len() == 1);
+    assert!(b.len() == 1);
+
+    let aa = a.get(0).unwrap();
+    let bb = b.get(0).unwrap();
+
+    assert_eq!(bb.2, 456);
+    assert_eq!(bb.0, 3);
+    assert_eq!(bb.1, 6);
+
+    assert_eq!(aa.2, 123);
+    assert_eq!(aa.0, 4);
+    assert_eq!(aa.1, 7);
 }
